84d5c874e6933c4c68bedddb9b191f5c4ed4c540
VladIgnat67 <vldslvbolotov@gmail.com>
2023-10-02 16:52:34 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,84 @@
+// Event Loop -
+
+// Call Stack - 
+// Wep API - 
+// CallBack Queue - 
+
+
+// console.log('object');
+// console.log('array');
+// function getData() {
+//     const data = [];
+//     return data;
+// }
+
+// getData()
+
+// const li = document.createElement('h1')
+
+// document.body.innerHTML = `<div>
+//     ${li}
+// </div>`;
+
+
+// function firstFun() {
+//     console.log('log1');
+//     console.log('log2');
+// }
+// function secondFun() {
+//     console.log('log3');
+//     firstFun()
+// }
+// secondFun()
+
+// const intervl = setInterval;
+// function stopInterval() {
+//     clearInterval(intervl) 
+// }
+// function timer() {
+//     let count = 1;
+//     console.log(count++);
+// }
+// setTimeout(() => {
+//     stopInterval();
+// }, 4000)
+
+// console.log('object');
+// console.log('object');
+// setTimeout(() => {
+//     console.log('Macro');
+// });
+// console.log('object');
+// console.log('object');
+// console.log('object');
+
+// console.log('start');
+
+// function fun() {
+//     setTimeout(() =>{
+//         console.log('TimeOut');
+//     }, 2000);
+//     console.log('fun finish');
+// }
+
+// console.log('object');
+// fun()
+// console.log('end');
+
+// function fn1() {
+//     console.log('1 msg');
+// }
+// function fn2() {
+//     console.log('2 msg');
+// }
+// function fn3() {
+//     console.log('3 msg');
+// }
+
+// function callAllMsg() {
+//     fn1();
+//     setTimeout(fn2, 1000);
+//     fn3();
+// }
+// callAllMsg()
+
